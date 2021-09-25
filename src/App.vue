ed0bc5b44596c7bf00dd54ba4d02f17a89ac9f5f
William <tony05078088@gmail.com>
2021-09-25 10:38:26 +0800

    docker編寫完成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <template>
   <div id="nav">
-   123
+    <div class="docker">
+      <span class="docker__item docker__item--active">
+        首頁
+        <i class="twicon-td-flag"></i>
+      </span>
+      <span class="docker__item"
+        >購物車
+        <i class="twicon-gate-cks"></i>
+      </span>
+      <span class="docker__item">
+        訂單
+        <i class="twicon-youbike"></i>
+      </span>
+      <span class="docker__item"
+        >我的
+        <i class="twicon-tea-pot"></i>
+      </span>
+    </div>
   </div>
-
 </template>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.docker {
+  display: flex;
+  position: absolute;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  &__item {
+    flex: 1;
+    text-align: center;
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+}
+i {
+  font-size: 0.35rem;
+  display: block;
+  margin: 0.02rem 0 0.02rem 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+/* eslint-disable quotes */
+import "normalize.css";
+import { createApp } from "vue";
+import '../public/twicon/twicon.css';
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./style/base.scss";
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
