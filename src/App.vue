80ea7fc3cb3ab03ee046c0544f0403c7112e63cc
William <tony05078088@gmail.com>
2021-09-30 23:59:10 +0800

    banner添加 利用padding-bottom 設定防抖

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
   <div id="nav">
+    <div class="wrapper">
+      <div class="position">
+        <span class="position__left">
+          <i class="twicon-beer"></i>
+        </span>
+        國立中央大學志道樓10層
+        <span class="position_right">
+          <i class="twicon-tapioca"></i>
+        </span>
+      </div>
+      <div class="search">
+        <i class="twicon-couplets"></i>
+        <span class="search__text">
+          酷炫會員商店優惠商品
+        </span>
+      </div>
+      <div class="banner">
+        <img
+          class="banner__img"
+          src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+        />
+      </div>
+    </div>
     <div class="docker">
       <span class="docker__item docker__item--active">
         首頁
@@ -22,6 +45,65 @@
 </template>
 
 <style lang="scss" scoped>
+@import "./style/variables.scss";
+@import "./style/mixins.scss";
+.wrapper {
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0.5rem;
+  right: 0;
+  padding: 0 0.18rem;
+}
+.search {
+  vertical-align: middle;
+  line-height: 0.32rem;
+  background: #f5f5f5;
+  color: #b7b7b7;
+  border-radius: 0.16rem;
+  font-size: 0.14rem;
+  i {
+    position: relative;
+    top: 0.06rem;
+    padding: 0 0.12rem 0 0.16rem;
+    font-size: 0.3rem;
+    display: inline-block;
+    margin-right: 0.05rem;
+  }
+  &__text {
+    display: inline-block;
+  }
+}
+.banner {
+  height: 0;
+  overflow: hidden;
+  //螢幕寬度的25.4%
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
+  }
+}
+.position {
+  padding: 0.16rem 0;
+  background: chartreuse;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
+  text-align: center;
+  color: $content-fontcolor;
+  //防止內容過多造成換行,此效果可達成多字省略
+  @include ellipse;
+  &__left {
+    position: relative;
+    top: 0.01rem;
+  }
+  i {
+    font-size: 0.3rem;
+    display: inline-block;
+    margin-right: 0.05rem;
+  }
+}
+
 .docker {
   display: flex;
   position: absolute;
@@ -30,7 +112,8 @@
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border-top: 1px solid #f1f1f1;
+  color: $content-fontcolor;
+  border-top: 0.01rem solid #f1f1f1;
   &__item {
     flex: 1;
     text-align: center;
@@ -38,10 +121,10 @@
       color: #1fa4fc;
     }
   }
-}
-i {
-  font-size: 0.35rem;
-  display: block;
-  margin: 0.02rem 0 0.02rem 0;
+  i {
+    font-size: 0.35rem;
+    display: block;
+    margin: 0.02rem 0 0.02rem 0;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import '../public/twicon/twicon.css';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./style/base.scss";
+import "./style/index.scss";
 createApp(App)
   .use(store)
   .use(router)
