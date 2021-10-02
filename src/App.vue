d5ba2eead7a3eb780ceff8b841fce5c1e29f2c3d
William <tony05078088@gmail.com>
2021-10-02 10:05:56 +0800

    圖標列表佈局

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,15 @@
           src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
         />
       </div>
+      <div class="icons">
+        <div v-for="icon in icons" :key="icon" class="icons__item">
+          <img :src="icon.imgsSrc" class="icons__item__img" />
+          <p class="icons__item__desc">{{ icon.title }}</p>
+        </div>
+      </div>
+      <div class="gap">
+
+      </div>
     </div>
     <div class="docker">
       <span class="docker__item docker__item--active">
@@ -44,6 +53,21 @@
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      icons: [
+        {
+          title: '超市便利',
+          imgsSrc: ['http://www.dell-lee.com/imgs/vue3/超市.png']
+        }
+      ]
+    };
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 @import "./style/variables.scss";
 @import "./style/mixins.scss";
@@ -63,6 +87,7 @@
   color: #b7b7b7;
   border-radius: 0.16rem;
   font-size: 0.14rem;
+  margin-bottom: 0.12rem;
   i {
     position: relative;
     top: 0.06rem;
@@ -83,6 +108,30 @@
   &__img {
     width: 100%;
   }
+}
+.icons {
+  display: flex;
+  margin-top: .16rem;
+  flex-wrap: wrap;
+  &__item {
+    width: 20%;
+    &__img {
+      width: 0.4rem;
+      height: 0.4rem;
+      display: block;
+      margin: 0 auto;
+    }
+    &__desc {
+      margin: .06rem 0 .16rem 0;
+      text-align: center;
+      color:$content-fontcolor;
+    }
+  }
+}
+.gap {
+  height:.1rem;
+  background: $content-bgColor;
+  margin: 0 -.18rem
 }
 .position {
   padding: 0.16rem 0;
@@ -113,7 +162,7 @@
   width: 100%;
   height: 0.49rem;
   color: $content-fontcolor;
-  border-top: 0.01rem solid #f1f1f1;
+  border-top: 0.01rem solid $content-bgColor;
   &__item {
     flex: 1;
     text-align: center;
